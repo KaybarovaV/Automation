7b03cf3520567d600f82229514e2ab7e22248ea6
kaybarovav <veronika.8686@mail.ru>
2024-05-27 15:13:24 +0700

    lesson_3_v2

--- Lesson_3/z_smartphone.py
+++ Lesson_3/z_smartphone.py
@@ -3,6 +3,5 @@ class Smartphone:
         self.ph_brand = ph_brand
         self.ph_model = ph_model
         self.ph_number = ph_number
-   # def smart(self):
-    #    print(self.ph_brand, "-", self.ph_model, ".", self.ph_number, ".")
+
 
